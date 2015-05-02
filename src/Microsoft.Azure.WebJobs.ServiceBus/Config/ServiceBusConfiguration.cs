17f55e287e0c785144bc127029852d67e1099750
Mathew Charles <mathewc@microsoft.com>
2015-05-01 19:28:15 -0700

    Enabling 3rd Party Extensibility

---
+++ src/Microsoft.Azure.WebJobs.ServiceBus/Config/ServiceBusConfiguration.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Azure.WebJobs.Host.Executors;
+
+namespace Microsoft.Azure.WebJobs.ServiceBus
+{
+    /// <summary>
+    /// Configuration options for the ServiceBus extension.
+    /// </summary>
+    public class ServiceBusConfiguration
+    {
+        private bool _connectionStringSet;
+        private string _connectionString;
+
+        /// <summary>
+        /// Gets or sets the Azure ServiceBus connection string.
+        /// </summary>
+        public string ConnectionString
+        {
+            get
+            {
+                if (!_connectionStringSet)
+                {
+                    _connectionString = AmbientConnectionStringProvider.Instance.GetConnectionString(ConnectionStringNames.ServiceBus);
+                    _connectionStringSet = true;
+                }
+
+                return _connectionString;
+            }
+            set
+            {
+                _connectionString = value;
+                _connectionStringSet = true;
+            }
+        }
+    }
+}
